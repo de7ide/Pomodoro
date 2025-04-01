5cffc16fd9b08277f54e0428e415ce4775944bc8
Des8de <dess8de@gmail.com>
2025-04-01 12:36:46 +0300

    db&cache value to settings

--- settings.py
+++ settings.py
@@ -2,4 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlite_db: str = "pomodoro.sqlite"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_USER: str = "admin"
+    DB_PASSWORD: str = "password"
+    DB_NAME: str = "pomodoro"
+    CACHE_HOST: str = "localhost"
+    CACHE_PORT: int = 6379
+    CACHE_DB: int = 0
