5bb1a6a76118ad32cb0367a7367780eea082876c
Des8de <dess8de@gmail.com>
2025-05-18 17:52:45 +0300

    feature: (Auth, User) add jwt auth and refactoring task schema

--- handlers/tasks.py
+++ handlers/tasks.py
@@ -1,12 +1,12 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
-from depends import get_task_service, get_tasks_repo
-from schemas.task import TaskSchema
+from depends import get_task_service, get_tasks_repo, get_request_user_id
+from exception import TaskNotFound
+from schemas import TaskCreateSchema, TaskSchema
 from repository import TaskRepo
 from services import TaskService
-
 
 
 router = APIRouter(prefix="/task", tags=["task"])
@@ -19,23 +19,42 @@ async def get_tasks(
     return task_services.get_tasks()
 
 
-
 @router.post("/create_task", response_model=TaskSchema)
-async def task_app(task: TaskSchema,
-                   task_repo: Annotated[TaskRepo, Depends(get_tasks_repo)]):
-    task_id = task_repo.create_task(task)
-    task.id = task_id
+async def task_app(
+    body: TaskCreateSchema,
+    task_service: Annotated[TaskService, Depends(get_task_service)],
+    user_id: int = Depends(get_request_user_id)
+):
+    task = task_service.create_task(body, user_id)
     return task
 
 
 @router.patch("/{task_id}", response_model=TaskSchema)
 async def patch_task(task_id: int,
                      name: str,
-                     task_repo: Annotated[TaskRepo, Depends(get_tasks_repo)]):
-    return task_repo.update_name(task_id, name)
+                     task_service: Annotated[TaskService, Depends(get_task_service)],
+                     user_id: int = Depends(get_request_user_id)):
+    try:
+        return task_service.update_task_name(
+            task_id=task_id,
+            name=name,
+            user_id=user_id
+        )
+    except TaskNotFound as e:
+        raise HTTPException(
+            status_code=404,
+            detail=e.detail
+        )
+
 
 @router.delete("/del_task")
 async def delete_task(task_id: int,
-                      task_repo: Annotated[TaskRepo, Depends(get_tasks_repo)]):
-    task_repo.delete_task(task_id)
-    return {"message": "ZAEB"}
+                      task_service: Annotated[TaskService, Depends(get_task_service)],
+                      user_id: int = Depends(get_request_user_id)):
+    try:
+        task_service.delete_task(task_id=task_id, user_id=user_id)
+    except TaskNotFound as e:
+        raise HTTPException(
+            status_code=404,
+            detail=e.detail
+        )
--- services/user.py
+++ services/user.py
@@ -1,20 +1,16 @@
-import string
-from random import choice
 from dataclasses import dataclass
 
 from repository.user import UserRepo
+from services.auth import AuthServices
 from schemas import UserLoginSchema
 
 
 @dataclass
 class UserService:
     user_repo: UserRepo
+    auth_service: AuthServices
 
     def create_user(self, username: str, password: str) -> UserLoginSchema:
-        access_token = self._generate_access_token()
-        user = self.user_repo.create_user(username=username, password=password, access_token=access_token)
-        return UserLoginSchema(user_id=user.id, access_token=user.access_token)
-
-    @staticmethod
-    def _generate_access_token() -> str:
-        return ''.join(choice(string.ascii_uppercase + string.digits) for _ in range(10))
+        user = self.user_repo.create_user(username=username, password=password)
+        access_token = self.auth_service.generate_access_token(user_id=user.id)
+        return UserLoginSchema(user_id=user.id, access_token=access_token)
